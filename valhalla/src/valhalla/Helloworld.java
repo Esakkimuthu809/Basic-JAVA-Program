f87bc92ca3734e539a8ade2adfa7605e148703af
esakimuthu-flash <75492279+esakimuthu-flash@users.noreply.github.com>
2021-03-01 10:22:01 +0530

    this is my first JAVA repository commit

---
+++ valhalla/src/valhalla/Helloworld.java
@@ -0,0 +1,13 @@
+package valhalla;
+
+public class Helloworld {
+  
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="hi";
+		 String Dog="Hello";
+		System.out.println(a);
+         System.out.println(Dog);
+	}
+
+}
